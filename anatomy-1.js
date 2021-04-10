6dfee548bcc8c6cc7add84bf09120f54e0574908
thiagogimenes <thiagoaticus@gmail.com>
2021-04-10 17:05:47 -0300

    Inicio do Curso

---
+++ anatomy-1.js
@@ -0,0 +1,31 @@
+// Function declaration
+
+// função que não recebe nenhum parametro e não retorna nada
+function sayHello() {
+    console.log('Hello!')
+}
+
+sayHello()
+
+//função que recebe parametro, mas não retorna nada
+function sayHelloTo(name) {
+    console.log(`Hello ${name}!`)
+}
+
+sayHelloTo('Mike')
+
+// função que não recebe parametro, mas retorna alguma coisa.
+function returnHi() {
+    return 'Hi!'
+}
+
+const greeting = returnHi()
+console.log(greeting)
+console.log(returnHi())
+
+// funação que recebe parametro e retorna alguma coisa.
+function returnHiTo(name) {
+    return `Hi ${name}!`
+}
+
+console.log(returnHiTo('John'))
---
+++ foundation/before.js
@@ -0,0 +1,17 @@
+//1. Pre-requisitos
+
+//-> Basic know of JS
+// ---> Data types: number, string, boolean, array and object
+// ---> Operatiors (+, -, *, /, &&, ||, >=, <=, ...)
+// ---> If, For, While
+
+// 2. Node
+// -> NodeJS (v14.6.0)
+
+// 3. Visual Studio Code
+// -> Code Runner
+// --> Code-runner: Clear Previous Output
+// --> Ctrl + Alt + n (run code)
+// --> Ctrl + Alt + m (stop the running code)
+
+console.log('test');
